012547481290a9d5bb5de162512d260178037eac
masked <37945842+Maskeddd@users.noreply.github.com>
2022-05-31 21:10:50 +1000

    fix: className instead of class

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -10,41 +10,41 @@ export default function Skills() {
           <div className="bg-Surface1 p-4 rounded-lg flex flex-col items-center gap-2.5 flex-grow">
             Languages
             <div className="flex flex-row text-2xl gap-1.5">
-              <i title="JavaScript" class="devicon-javascript-plain"></i>
-              <i title="Python" class="devicon-python-plain"></i>
-              <i title="Solidity" class="devicon-solidity-plain"></i>
-              <i title="Markdown" class="devicon-markdown-original"></i>
-              <i title="HTML" class="devicon-html5-plain"></i>
-              <i title="CSS" class="devicon-css3-plain"></i>
+              <i title="JavaScript" className="devicon-javascript-plain"></i>
+              <i title="Python" className="devicon-python-plain"></i>
+              <i title="Solidity" className="devicon-solidity-plain"></i>
+              <i title="Markdown" className="devicon-markdown-original"></i>
+              <i title="HTML" className="devicon-html5-plain"></i>
+              <i title="CSS" className="devicon-css3-plain"></i>
             </div>
           </div>
           <div className="bg-Surface1 p-4 rounded-lg flex flex-col items-center gap-2.5 flex-grow">
             Frameworks
             <div className="flex flex-row text-2xl gap-1.5">
-              <i title="Next.js" class="devicon-nextjs-original"></i>
-              <i title="React" class="devicon-react-original"></i>
-              <i title="Express.js" class="devicon-express-original"></i>
-              <i title="Tailwind CSS" class="devicon-tailwindcss-plain"></i>
+              <i title="Next.js" className="devicon-nextjs-original"></i>
+              <i title="React" className="devicon-react-original"></i>
+              <i title="Express.js" className="devicon-express-original"></i>
+              <i title="Tailwind CSS" className="devicon-tailwindcss-plain"></i>
             </div>
           </div>
           <div className="bg-Surface1 p-4 rounded-lg flex flex-col items-center gap-2.5 flex-grow">
             Databases
             <div className="flex flex-row text-2xl gap-1.5">
-              <i title="MongoDB" class="devicon-mongodb-plain"></i>
+              <i title="MongoDB" className="devicon-mongodb-plain"></i>
             </div>
           </div>
           <div className="bg-Surface1 p-4 rounded-lg flex flex-col items-center gap-2.5 flex-grow">
             DevOps
             <div className="flex flex-row text-2xl gap-1.5">
-              <i title="Git" class="devicon-git-plain"></i>
-              <i title="GitHub" class="devicon-github-original"></i>
+              <i title="Git" className="devicon-git-plain"></i>
+              <i title="GitHub" className="devicon-github-original"></i>
             </div>
           </div>
           <div className="bg-Surface1 p-4 rounded-lg flex flex-col items-center gap-2.5 flex-grow">
             Design
             <div className="flex flex-row text-2xl gap-1.5">
-              <i title="Illustrator" class="devicon-illustrator-plain"></i>
-              <i title="Adobe XD" class="devicon-xd-plain"></i>
+              <i title="Illustrator" className="devicon-illustrator-plain"></i>
+              <i title="Adobe XD" className="devicon-xd-plain"></i>
             </div>
           </div>
         </div>
